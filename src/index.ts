b55e5dcbe57721321c4c58d50fc0ca5d2636d7f1
H4ckW1s3r <clementix01@gmail.com>
2024-05-24 10:28:27 +0200

    v1

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,92 @@
+import { v4 as uuidV4 } from "uuid";
+
+// Creating the type
+type Task = {
+  id: string,
+  title: string,
+  completed: boolean,
+  createdAt: Date
+};
+
+// DOM Elements
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.querySelector<HTMLFormElement>("#new-task-form");
+const input = document.querySelector<HTMLInputElement>("#new-task-title");
+let tasks: Task[] = loadTasks();
+tasks.forEach(addListItem);
+
+// On th submit event from the form
+form?.addEventListener("submit", e => {
+  // Prevent refreshing the page
+  e.preventDefault();
+  // Check if input exist and contain value
+  if(!input?.value) return;
+
+  // We create the new task
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+
+  // We add the new task to the list
+  tasks.push(newTask);
+  // And display it
+  addListItem(newTask);
+  // Save every task in the localStorage
+  saveTasks();
+  // We empty the input value
+  input.value = "";
+});
+
 /**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+ * Display the new task and attach the change event
+ * @param task
+ */
+function addListItem(task:Task) {
+  // We create every element we wanted
+  const item = document.createElement("li");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  const btnSuppr = document.createElement("button");
+  // We attach button elements
+  btnSuppr.innerText = "Delete";
+  btnSuppr.title = "Delete";
+  btnSuppr.classList.add("btn-delete-task");
+  btnSuppr.addEventListener("click", () => {
+    // We filter the tasks without the one deleted
+    tasks = tasks.filter(t => t.id !== task.id);
+    // We clear the content of the list
+    if(!!list) list.innerHTML = "";
+    saveTasks();
+    // We display them again
+    tasks.forEach(addListItem);
+  });
+  // We attach the change event on the checkbox
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked;
+    saveTasks();
+  });
+  checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
+  // We display it as one block in the list
+  label.append(task.title);
+  item.append(checkbox, label, btnSuppr);
+  list?.append(item);
+}
 
-import confetti from 'canvas-confetti';
+/* Save tasks in the localStorage */
+function saveTasks(){
+  localStorage.setItem("done-not-tasks", JSON.stringify(tasks));
+}
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+/**
+ * Gets the tasks from localStorage and return them
+ * @returns {Task[]}
+ */
+function loadTasks() : Task[] {
+  const tasksJSON = localStorage.getItem("done-not-tasks");
+  if(!tasksJSON) return [];
+  return JSON.parse(tasksJSON);
+}
